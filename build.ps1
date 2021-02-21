8cbc916e69b5b81c1f72595748c1fc9504c75f0a
Waldemar Tomme <waldetomme@gmail.com>
2021-02-21 06:32:14 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,13 @@
 #!/usr/bin/env pwsh
+$ErrorActionPreference = 'Stop'
+
 $now = [DateTime]::Now
 $versionSuffix = "-pre$($now.ToString("yyyyMMddHHmmss"))"
-dotnet pack ./DarkLink.Generators/DarkLink.Generators.csproj --version-suffix $versionSuffix
+
+New-Item ./output -ItemType Directory -Force | Out-Null
+Remove-Item ./output/* -Recurse -Force | Out-Null
+dotnet pack ./DarkLink.Generators/DarkLink.Generators.csproj `
+    --configuration Release `
+    --version-suffix $versionSuffix `
+    --output ./output `
+    --verbosity normal
