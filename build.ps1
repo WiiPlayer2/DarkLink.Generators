91879c77ee8028454014e23c6a9930a4c88fdce8
Waldemar Tomme <waldetomme@gmail.com>
2021-02-21 08:21:52 +0100

    Fix build

--- build.ps1
+++ build.ps1
@@ -6,8 +6,11 @@ $versionSuffix = "-pre$($now.ToString("yyyyMMddHHmmss"))"
 
 New-Item ./output -ItemType Directory -Force | Out-Null
 Remove-Item ./output/* -Recurse -Force | Out-Null
+dotnet build ./DarkLink.Generators/DarkLink.Generators.csproj `
+    --configuration Release `
+    --verbosity normal
 dotnet pack ./DarkLink.Generators/DarkLink.Generators.csproj `
     --configuration Release `
     --version-suffix $versionSuffix `
     --output ./output `
-    --verbosity normal
+    --verbosity minimal
