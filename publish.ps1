4dc329cfc2fa1a7ebb7bcc2d057e667b2775439b
Waldemar Tomme <waldetomme@gmail.com>
2021-02-21 08:25:50 +0100

    Fix location

--- publish.ps1
+++ publish.ps1
@@ -1,8 +1,9 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = 'Stop'
 
-Set-Location ./output
+Push-Location ./output
 dotnet nuget push `
     '*.nupkg' `
     --api-key $env:NUGET_API_KEY `
     --source $env:NUGET_SOURCE
+Pop-Location
