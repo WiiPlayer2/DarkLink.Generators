e6bcaa3b98d79cfcc2826865e381ac8c7d4b62c4
Waldemar Tomme <waldetomme@gmail.com>
2021-03-13 13:19:28 +0100

    Implement Action matcher

--- DarkLink.EnumMatcher/Generator.cs
+++ DarkLink.EnumMatcher/Generator.cs
@@ -1,9 +1,11 @@
 ﻿using DarkLink.Generators.Shared;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace DarkLink.EnumMatcher
 {
@@ -21,6 +23,7 @@ namespace DarkLink.EnumMatcher
 
             var enumTypeSymbols = CollectEnumTypes(context.Compilation);
             var enumInfos = enumTypeSymbols.Select(MapEnumType);
+            enumInfos.Foreach(enumInfo => GenerateMatcher(enumInfo, context));
         }
 
         public void Initialize(GeneratorInitializationContext context)
@@ -53,9 +56,38 @@ namespace DarkLink.EnumMatcher
             return enumTypeSymbols;
         }
 
-        private void GenerateMatcher(EnumInfo enumInfo)
+        private void GenerateMatcher(EnumInfo enumInfo, GeneratorExecutionContext context)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+            sb.AppendLine($"using System;");
+            sb.AppendLine($"namespace {enumInfo.EnumTypeSymbol.ContainingNamespace} {{");
+            sb.AppendLine($"    internal static class {enumInfo.EnumTypeSymbol.Name}Matcher {{");
+
+            // Generate Action
+            sb.AppendLine($"        public static void Match(");
+            sb.Append($"            this {enumInfo.EnumTypeSymbol} thisEnum");
+            foreach (var field in enumInfo.Fields)
+            {
+                sb.AppendLine($",");
+                sb.Append($"            Action on{field.Name}");
+            }
+            sb.AppendLine($") {{");
+            sb.AppendLine($"            switch(thisEnum) {{");
+            foreach (var field in enumInfo.Fields)
+            {
+                sb.AppendLine($"                case {field}:");
+                sb.AppendLine($"                    on{field.Name}();");
+                sb.AppendLine($"                    break;");
+            }
+            sb.AppendLine($"                default:");
+            sb.AppendLine($"                    throw new NotSupportedException();");
+            sb.AppendLine($"            }}");
+            sb.AppendLine($"        }}");
+
+            sb.AppendLine($"    }}");
+            sb.AppendLine($"}}");
+            var sourceText = SourceText.From(sb.ToString(), Encoding.UTF8);
+            context.AddSource($"{enumInfo.EnumTypeSymbol}.cs", sourceText);
         }
 
         private EnumInfo MapEnumType(INamedTypeSymbol enumTypeSymbol)
